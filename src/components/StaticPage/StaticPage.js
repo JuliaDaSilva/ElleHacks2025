d091f48fd187e7d9e9a64c662074acbd6b7a1a18
Julia Da Silva <julia6pds@gmail.com>
2024-10-06 01:53:58 -0400

    Add files via upload

---
+++ src/components/StaticPage/StaticPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StaticPage.css';
+
+const StaticPage = () => {
+    return (
+        <div className="static-page">
+            <h1>Event Date: Feb 14-16</h1>
+            <p>Applications Coming Soon!</p>
+        </div>
+    );
+};
+
+export default StaticPage;
