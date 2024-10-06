1dc31c3f087a4469363a45da404512e415faa07f
Julia Da Silva <julia6pds@gmail.com>
2024-10-06 11:58:40 -0400

    Add files via upload

--- src/components/StaticPage/StaticPage.js
+++ src/components/StaticPage/StaticPage.js
@@ -3,11 +3,12 @@ import './StaticPage.css';
 
 const StaticPage = () => {
     return (
-        <div className="static-page">
-            <h1>Event Date: Feb 14-16</h1>
-            <p>Applications Coming Soon!</p>
+        <div className="h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold">Event Date: Feb 14-16</h1>
+            <p className="mt-4 text-lg">Applications Coming Soon!</p>
         </div>
     );
 };
 
 export default StaticPage;
+
