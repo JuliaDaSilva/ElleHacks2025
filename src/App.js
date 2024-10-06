f02371dee02149448730eee8acd01865f02ef140
Julia Da Silva <julia6pds@gmail.com>
2024-10-06 17:45:08 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import StaticPage from './components/StaticPage/StaticPage';
+
+function App() {
+  return (
+      <div className="App">
+          <StaticPage />
+      </div>
+  );
+}
+
+export default App;
